13365c0dab432aa76da693ef880b8c771a4f8b3b
Brian Kiel <briankiel@gmail.com>
2025-05-25 17:59:30 -0500

    updating stuff for the api types file

--- frontend-next/src/types/api.ts
+++ frontend-next/src/types/api.ts
@@ -14,6 +14,14 @@ export type Performance = {
   internal_id: number;
   showDate: string;
   external_venue_id: string;
+
+  // Add this:
+  venue?: {
+    venue_name_display: string;
+    city_name: string;
+    locality_name: string;
+    country_name: string;
+  };
 };
 
 export type Artist = {
